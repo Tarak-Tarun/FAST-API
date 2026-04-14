8be1899635ee01194ba155736ed1a6a917bcaac8
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-14 15:05:18 +0530

    Task inclusing optional field validator and nested model Added

--- APR14/schemas/user_schema.py
+++ APR14/schemas/user_schema.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel, Field,model_validator  #type: ignore
-class Product(BaseModel):
-    name: str = Field(min_length=3)
-    price:int=Field(ge=100)
-    quantity:int=Field(ge=1)
-    @model_validator(mode="before")
-    def before_validation(cls,values):
-        values["name"]=values.get("name").upper()
-        return values
-    @model_validator(mode="after")
-    def after_validation(self):
-        if self.price < 100:
-            raise ValueError("Price must be at least 100")
-        return self
+from pydantic import field_validator,BaseModel #type: ignore
+from  typing import Optional
+class Address(BaseModel):
+    city:str
+    pincode:int
+    state:str
 
+class Product(BaseModel):
+    name:str
+    price:float
+    quantity:int
+    address:Address
+    description:Optional[str]=None
+    @field_validator("name")
+    def name_validator(cls,value):
+        if not value.replace(" ","").isalpha():
+            raise ValueError("Name must contain only letters")
+        return value
