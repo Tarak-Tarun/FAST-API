e962b613811aea4fc058c1e838c7e4f77f0ab672
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-14 12:28:13 +0530

    Field min_length re max_length ge le

--- APR14/routers/User.py
+++ APR14/routers/User.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter #type: ignore
-from schemas.user_schema import User #type: ignore
+from schemas.user_schema import Product #type: ignore
 router = APIRouter()
-@router.post("/users",status_code=201,response_model=User)
-def create_user(user: User):
-    return user
+@router.post("/prodcuts",status_code=201,response_model=Product)
+def products(product: Product):
+    return product
--- APR14/schemas/user_schema.py
+++ APR14/schemas/user_schema.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field #type: ignore
-class User(BaseModel):
-    name:str=Field(min_length=3,max_length=50)
-    email:str =Field(min_length=5,max_length=50)
-    age:int=Field(gt=0,lt=120)
-
+class Product(BaseModel):
+    name: str = Field(min_length=3)
+    price:int=Field(ge=100)
+    quantity:int=Field(ge=1)
