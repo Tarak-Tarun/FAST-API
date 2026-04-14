3a8a1e77752d476c6b92738b9c121686cec80e9c
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-14 12:45:07 +0530

    Model validator Added

--- APR14/routers/User.py
+++ APR14/routers/User.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter #type: ignore
 from schemas.user_schema import Product #type: ignore
 router = APIRouter()
-@router.post("/prodcuts",status_code=201,response_model=Product)
+@router.post("/prodcuts")
 def products(product: Product):
     return product
--- APR14/schemas/user_schema.py
+++ APR14/schemas/user_schema.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel, Field #type: ignore
+from pydantic import BaseModel, Field,model_validator  #type: ignore
 class Product(BaseModel):
     name: str = Field(min_length=3)
     price:int=Field(ge=100)
     quantity:int=Field(ge=1)
+    @model_validator(mode="before")
+    def before_validation(cls,values):
+        values["name"]=values.get("name").upper()
+        return values
+    @model_validator(mode="after")
+    def after_validation(self):
+        if self.price < 100:
+            raise ValueError("Price must be at least 100")
+        return self
+
